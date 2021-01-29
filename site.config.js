4eb3620ff7bd65057b9670072bedc0fef69d8319
mola1129 <ms.mola1129@gmail.com>
2021-01-29 10:55:03 +0900

    Update a site config file.

--- site.config.js
+++ site.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '5b274ef08de84a8cb6f8ba7e0e19c13c',
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Mola1129',
+  domain: 'mola1129.dev',
+  author: 'mola1129',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'mola1129',
+  socialImageTitle: 'mola1129',
+  socialImageSubtitle: 'mola1129! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'mola1129',
+  github: 'mola1129',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
